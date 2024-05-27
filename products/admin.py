b598eaf6d076ceddec901872b9f63a2ef667584f
Logan  Carlow <logan.carlow@gmail.com>
2024-05-27 14:24:23 +0000

    feat(products): add basic products view

--- products/admin.py
+++ products/admin.py
@@ -1,17 +1,30 @@
 from django.contrib import admin
 from .models import Product, Category, Manufacturer
+
 # Register your models here.
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'friendly_name')
+    list_display = ("name", "friendly_name")
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'manufacturer', 'price', 'rating')
-    search_fields = ('name', 'category__name', 'manufacturer__name')
-    list_filter = ('category', 'manufacturer', 'rating')
+    list_display = (
+        "sku",
+        "name",
+        "category",
+        "manufacturer",
+        "price",
+        "rating",
+        "image",
+    )
+    ordering = ('sku',)
+    search_fields = ("name", "category__name", "manufacturer__name")
+    list_filter = ("category", "manufacturer", "rating")
+
 
 @admin.register(Manufacturer)
 class ManufacturerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'website')
+    list_display = ("name", "website")
