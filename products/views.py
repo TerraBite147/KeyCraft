966e6182a46fb592cf15a8ad9cbcb34e65d83a55
Logan  Carlow <logan.carlow@gmail.com>
2024-05-31 10:47:18 +0000

    feat(products): add search functionality for products

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -7,9 +9,23 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search query")
+                return redirect(reverse('products'))
+
+            # Regex pattern for whole word matching
+            regex_pattern = r'\b' + query + r'\b'
+            queries = Q(name__iregex=regex_pattern) | Q(description__iregex=regex_pattern)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
