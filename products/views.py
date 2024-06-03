b433e015155054e115ac89f207162278aafa727d
Logan  Carlow <logan.carlow@gmail.com>
2024-06-03 14:30:44 +0000

    feat(products): add catergory and manufacturer filtering

--- products/views.py
+++ products/views.py
@@ -7,43 +7,64 @@ from .models import Product, Category, Manufacturer
 
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """A view to show all products, including sorting and search queries"""
 
     products = Product.objects.all()
     query = None
     categories = None
     manufacturer = None
+    sort = None
+    direction = None
 
     if request.GET:
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
+        if "sort" in request.GET:
+            sortkey = request.GET["sort"]
+            sort = sortkey
+            if sortkey == "name":
+                sortkey = "lower_name"
+                products = products.annotate(lower_name=Lower("name"))
+            elif sortkey == "manufacturer":
+                sortkey = "manufacturer__name"
+            if "direction" in request.GET:
+                direction = request.GET["direction"]
+                if direction == "desc":
+                    sortkey = f"-{sortkey}"
+            products = products.order_by(sortkey)
+
+        if "category" in request.GET:
+            categories = request.GET["category"].split(",")
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-        if 'manufacturer' in request.GET:
-            manufacturer = request.GET['manufacturer']
+        if "manufacturer" in request.GET:
+            manufacturer = request.GET["manufacturer"]
             if manufacturer:
                 products = products.filter(manufacturer__name__iexact=manufacturer)
 
-        if 'q' in request.GET:
-            query = request.GET['q']
+        if "q" in request.GET:
+            query = request.GET["q"]
             if not query:
                 messages.error(request, "You didn't enter any search query")
-                return redirect(reverse('products'))
+                return redirect(reverse("products"))
 
             # Regex pattern for whole word matching
-            regex_pattern = r'\b' + query + r'\b'
-            queries = Q(name__iregex=regex_pattern) | Q(description__iregex=regex_pattern)
+            regex_pattern = r"\b" + query + r"\b"
+            queries = Q(name__iregex=regex_pattern) | Q(
+                description__iregex=regex_pattern
+            )
             products = products.filter(queries)
 
+    current_sorting = f"{sort}_{direction}"
+
     context = {
-        'products': products,
-        'search_term': query,
-        'current_categories': categories,
-        'current_manufacturer': manufacturer,
+        "products": products,
+        "search_term": query,
+        "current_categories": categories,
+        "current_manufacturer": manufacturer,
+        "current_sorting": current_sorting,
     }
 
-    return render(request, 'products/products.html', context)
+    return render(request, "products/products.html", context)
 
 
 def product_detail(request, product_id):
