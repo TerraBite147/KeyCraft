38a60242c0db95c6c434aa44f421e39ecfad27ab
Logan  Carlow <logan.carlow@gmail.com>
2024-06-18 10:46:58 +0000

    feat(email): link to gmail smtp server

--- keycraft/settings.py
+++ keycraft/settings.py
@@ -191,9 +191,17 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # Email configuration
-DEFAULT_FROM_EMAIL = "keycraft@example.com"
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'keycraft@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
