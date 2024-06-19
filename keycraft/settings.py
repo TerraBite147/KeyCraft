bd46da3034be14c813b5675637c2751bf7f2b929
Logan  Carlow <logan.carlow@gmail.com>
2024-06-19 21:27:49 +0000

    bug(checkout): fix issue with order not totaling correctly

--- keycraft/settings.py
+++ keycraft/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from cloudinary_storage.storage import MediaCloudinaryStorage
 
 if os.path.isfile("env.py"):
     import env
